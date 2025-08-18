6ad14027c22278871fa0d0ece0bde856cb60c483
German <thewhitehaven04@gmail.com>
2025-08-18 20:43:15 +0200

    Reset password form

--- src/components/ResetPasswordForm.vue
+++ src/components/ResetPasswordForm.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { InputText } from 'primevue'
+import { resetPassword } from '@/services/auth.ts'
 
+const email = defineModel('email', { type: String })
+const isSuccessMessageShown = ref(false)
+
+const handleReset = () => {
+  resetPassword({ email })
+}
 </script>
 
 <template>
-  $END$
+  <Dialog title="Reset password">
+    <InputText type="password" v-model="email" />
+    <Button type="button" @click="handleReset">Reset</Button>
+    <Message v-if="isSuccessMessageShown" severity="success" size="small"
+      >Success! Check your E-mail</Message
+    >
+  </Dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
